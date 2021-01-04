8f3369987667d8636248cd8c565b4a098fa0177c
JL <909861220@qq.com>
2021-01-04 12:01:34 +0800

    docs-modiy

---
+++ docs/exercise/template.vue
@@ -0,0 +1,13 @@
+<template></template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  components: {},
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+</style>
